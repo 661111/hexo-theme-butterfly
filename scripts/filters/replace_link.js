4532aa623a731a9f7d95d2bf9d55011696785729
ImCaO <imcao@imcao.cn>
2022-06-22 17:11:02 +0800

    简化静态资源引用方式

--- scripts/filters/replace_link.js
+++ scripts/filters/replace_link.js
@@ -4,26 +4,19 @@
  * By ImCaO
  */
 
-'use strict'
+"use strict";
 
-hexo.extend.filter.register("markdown-it:renderer", function (md) {
-  md.use(require("markdown-it-replace-link"), {
-    replaceLink: function (link, env) {
-      const { replace_link } = hexo.theme.config
-      if (link.toString().startsWith(replace_link.alias)) {
-        return replace_link.replacement + link.toString().substring(replace_link.alias.length);
-      } else {
-        return link.toString();
-      }
-    },
-  });
-});
-
-hexo.extend.filter.register('before_post_render', function (data) {
-  const { replace_link } = hexo.theme.config
-  const cover = data.cover
-  if (cover && cover.toString().startsWith(replace_link.alias)) {
-    data.cover = replace_link.replacement + cover.toString().substring(replace_link.alias.length);
+hexo.extend.filter.register("before_post_render", function (data) {
+  const { replace_link } = hexo.theme.config;
+  const cover = data.cover;
+  if (cover) {
+    let num = cover.toString().lastIndexOf("/");
+    data.cover = replace_link.replacement + '/assets/covers/' + cover.toString().substring(num + 1);
   }
-  return data
-})
+  if (data) {
+    const reg = new RegExp(replace_link.alias, "g");
+    let num = data.source.lastIndexOf(".");
+    data.content = data.content.replace(reg, replace_link.replacement + '/' + data.source.substring(0, num));
+  }
+  return data;
+});
